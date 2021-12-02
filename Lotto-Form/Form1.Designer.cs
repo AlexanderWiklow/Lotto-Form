fe8fdb686aed89f54c580472eca7068077bfbefd
Alexander <a.wiklow@gmail.com>
2021-12-02 13:15:54 +0100

    Add files via upload

---
+++ Lotto-Form/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace Lotto_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtSeventh = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSixth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFifth = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFourth = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtThird = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbNew = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ett";
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(12, 206);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(63, 31);
+            this.txtFirst.TabIndex = 1;
+            // 
+            // txtSeventh
+            // 
+            this.txtSeventh.Location = new System.Drawing.Point(426, 206);
+            this.txtSeventh.Name = "txtSeventh";
+            this.txtSeventh.Size = new System.Drawing.Size(63, 31);
+            this.txtSeventh.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(426, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sju";
+            // 
+            // txtSixth
+            // 
+            this.txtSixth.Location = new System.Drawing.Point(357, 206);
+            this.txtSixth.Name = "txtSixth";
+            this.txtSixth.Size = new System.Drawing.Size(63, 31);
+            this.txtSixth.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(357, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sex";
+            // 
+            // txtFifth
+            // 
+            this.txtFifth.Location = new System.Drawing.Point(288, 206);
+            this.txtFifth.Name = "txtFifth";
+            this.txtFifth.Size = new System.Drawing.Size(63, 31);
+            this.txtFifth.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(288, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Fem";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtFourth
+            // 
+            this.txtFourth.Location = new System.Drawing.Point(219, 206);
+            this.txtFourth.Name = "txtFourth";
+            this.txtFourth.Size = new System.Drawing.Size(63, 31);
+            this.txtFourth.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(219, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Fyra";
+            // 
+            // txtThird
+            // 
+            this.txtThird.Location = new System.Drawing.Point(150, 206);
+            this.txtThird.Name = "txtThird";
+            this.txtThird.Size = new System.Drawing.Size(63, 31);
+            this.txtThird.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(150, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Tre";
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.Location = new System.Drawing.Point(81, 206);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(63, 31);
+            this.txtSecond.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(81, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Två";
+            // 
+            // cmbNew
+            // 
+            this.cmbNew.Location = new System.Drawing.Point(31, 261);
+            this.cmbNew.Name = "cmbNew";
+            this.cmbNew.Size = new System.Drawing.Size(194, 59);
+            this.cmbNew.TabIndex = 14;
+            this.cmbNew.Text = "Skapa ny lottorad";
+            this.cmbNew.UseVisualStyleBackColor = true;
+            this.cmbNew.Click += new System.EventHandler(this.cmbNew_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbNew);
+            this.Controls.Add(this.txtSecond);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtThird);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtFourth);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtFifth);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSixth);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSeventh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFirst);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.TextBox txtSeventh;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSixth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFifth;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFourth;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtThird;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSecond;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button cmbNew;
+    }
+}
+
---
+++ Lotto-Form/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lotto_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbNew_Click(object sender, EventArgs e)
+        {
+
+            //C# has a random class, which you can start with. Your requirement that each number is allowed only once requires some work, as this is no longer random. What you can do, is have an array for 8 numbers and the repeatedly call the next random number an check if it’s already in the filled part of your array. When you have filled all 8 slots of your array, you can stop filling it and print it out.
+
+            //instansiera random 
+            Random rnd = new Random();
+
+            int[] talArray = new int[7];
+
+            for (int i = 0; i < talArray.Length; i++)
+            {
+                int slumpTal = rnd.Next(0, 38);
+
+                for (int j = 0; j < talArray.Length; j++)
+                {
+                    if (talArray[j] == slumpTal )
+                    {
+                        slumpTal = rnd.Next(0, 38);
+                    }
+                }
+                
+                talArray[i] = slumpTal;
+            }
+
+            //Skriv ut till boxarna 
+            txtFirst.Text = Convert.ToString(talArray[0]);
+            txtSecond.Text = Convert.ToString(talArray[1]);
+            txtThird.Text = Convert.ToString(talArray[2]);
+            txtFourth.Text = Convert.ToString(talArray[3]);
+            txtFifth.Text = Convert.ToString(talArray[4]);
+            txtSixth.Text = Convert.ToString(talArray[5]);
+            txtSeventh.Text = Convert.ToString(talArray[6]);
+
+        }
+    }
+}
